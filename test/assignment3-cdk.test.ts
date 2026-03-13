34987109fe25d3fb4bff8e6b36ceb5405b496f4f
Rayraysub <zhangyuchong699@gmail.com>
2026-03-12 23:13:39 -0700

    Initial commit

---
+++ test/assignment3-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Assignment3Cdk from '../lib/assignment3-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/assignment3-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Assignment3Cdk.Assignment3CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
